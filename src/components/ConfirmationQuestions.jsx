72b37ccbba6dfb9e6e4f28fc633da7ef77128d40
Eddie Harris and James Hanley <james@>
2018-09-10 10:55:37 -0700

    add control logic

---
+++ src/components/ConfirmationQuestions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ConfirmationQuestions(){
+  return (
+    <div>
+      <p>Have you gone through all the steps on the Learn How to Program debugging lesson?</p>
+      <button>YES</button>
+    </div>
+  );
+}
+
+export default ConfirmationQuestions;
--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
+
 
 class NewTicketControl extends React.Component {
 
@@ -7,18 +11,27 @@ class NewTicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    //this.handleClick = this.handleClick.bind(this);
   }
-
-  handleClick(){
-    this.setState({formVisibleOnPage: true});
-    console.log('formVisibleOnPage is currently set to:' +this.state.formVisibleOnPage);
-  }
+  //setState method sets a new state instead of altering it.
+  //The only way we can update state outside of constructor() is by using the built in
+  //setStateMethod()
+  // handleClick(){
+  //   debugger;
+  //   this.setState({formVisibleOnPage: true});
+  //   console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
+  // }
 
   render(){
+    let currentlyVisisbleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisisbleContent = <NewTicketForm />;
+    }else{
+      currentlyVisisbleContent = <ConfirmationQuestions />;
+    }
     return (
       <div>
-        <p>This is the NewTicketControl component!</p>
-        <strong onClick={this.handleClick}>Press Me NOW</strong>
+        {currentlyVisisbleContent}
       </div>
     );
   }
